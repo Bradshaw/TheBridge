c6352e2a2f55e6b2118343e2e198ebb669c4b563
Kevin Bradshaw <kevin@naturalpad.fr>
2014-07-07 16:33:45 +0200

    Fixes hasty push

--- server/app.node.js
+++ server/app.node.js
@@ -5,14 +5,15 @@ var async = require("async");
 var net = require("net");
 
 
-var ship = require("ship");
+var ship = require("./ship");
 
 
 
 
 var server = net.createServer(function (socket) {
-	
-}
+
+});
 
 
 server.listen(1337, '0.0.0.0');
+console.log("Listening...");

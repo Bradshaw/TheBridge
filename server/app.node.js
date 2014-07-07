893a8aaeeab20b7c57a8f4877d7cbfb5258c92fd
Kevin Bradshaw <kevin@naturalpad.fr>
2014-07-07 16:25:53 +0200

    Adds skeleton structure

---
+++ server/app.node.js
@@ -0,0 +1,18 @@
+var _ = require("underscore");
+var lazy = require("lazy.js");
+var async = require("async");
+
+var net = require("net");
+
+
+var ship = require("ship");
+
+
+
+
+var server = net.createServer(function (socket) {
+	
+}
+
+
+server.listen(1337, '0.0.0.0');
---
+++ server/ship.js
@@ -0,0 +1,8 @@
+var _ = require("underscore");
+var lazy = require("lazy.js");
+var async = require("async");
+var ship = {}
+
+
+
+module.exports = ship
